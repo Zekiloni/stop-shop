341a7a2927f7e6af66298750c9ba194ca417fc01
Zekiloni <zekirija17@gmail.com>
2025-01-15 19:46:23 +0100

    - implementation of cart subject in shop service - implementation of add to cart - impl of remove from cart - impl of clear cart - implement of calculate cart total bill

--- src/app/core/shop.service.ts
+++ src/app/core/shop.service.ts
@@ -1,9 +1,12 @@
 import {Injectable} from '@angular/core';
 import {products} from './shop.data';
+import {BehaviorSubject} from 'rxjs';
+import {Product} from './shop.model';
 
 @Injectable()
 export class ShopService {
-
+  private cartSubject = new BehaviorSubject<Product[]>([]);
+  cart$ = this.cartSubject.asObservable();
 
   getAllProducts() {
     return products;
@@ -11,5 +14,28 @@ export class ShopService {
 
   getProductById(id: string) {
     return products.find(product => product.id === id);
+  }
+
+
+  addToCart(product: Product) {
+    const currentCart = this.cartSubject.getValue();
+    this.cartSubject.next([...currentCart, product]);
+  }
+
+  removeFromCart(productId: string) {
+    const updatedCart = this.cartSubject.getValue().filter(product => product.id !== productId);
+    this.cartSubject.next(updatedCart);
+  }
+
+  clearCart() {
+    this.cartSubject.next([]);
+  }
+
+  getCartItems() {
+    return this.cartSubject.getValue();
+  }
+
+  getCartTotal() {
+    return this.cartSubject.getValue().reduce((total, product) => total + product.price, 0);
   }
 }
